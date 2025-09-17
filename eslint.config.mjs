db37c6e33c35dc1888158b66e6810f73a6174a5b
codewithsaidul <codewithsaidul@gmail.com>
2025-09-17 11:50:24 +0600

    first nestjs backend setup completed

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+          printWidth: 100,
+          singleQuote: true,
+          trailingComma: 'all',
+
+          // 👇 এটা main fix
+          proseWrap: 'preserve',
+        },
+      ],
+      'no-multiple-empty-lines': ['error', { max: 10, maxEOF: 0 }],
+    },
+  },
+);
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT ?? 3000);
+  } catch (error) {
+    console.error("❌ Error during server startup:", error);
+  }
+}
+
+(async () => {
+  await bootstrap();
+})()
+
